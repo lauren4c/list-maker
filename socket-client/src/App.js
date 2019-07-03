07286b8ee45c8fcc94526e8982a6592cf6f18a97
Lauren Forcey <lauren4c@gmail.com>
2019-07-03 09:56:58 -0600

    removed submodule

---
+++ socket-client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import "./App.css";
+
+// Making the App component
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "localhost:4001",
+
+      ///
+      color: "white"
+      ///
+    };
+  }
+
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit("change color", this.state.color);
+  };
+
+  // render method that renders in code if the state is updated
+  render() {
+    return (
+      <div>
+        <p>Making sure this works</p>
+      </div>
+    );
+  }
+}
+
+export default App;
